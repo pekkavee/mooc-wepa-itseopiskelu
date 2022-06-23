0c86e48a81a30903fdbe30f49a40c0768932fa9d
pekkavee <pekkatapiovenalainen@gmail.com>
2022-06-23 15:29:07 +0300

    commit

--- osa01-Osa01_03.HelloRedirectLoop/src/main/java/HelloRedirectLoop.java
+++ osa01-Osa01_03.HelloRedirectLoop/src/main/java/HelloRedirectLoop.java
@@ -2,12 +2,40 @@
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class HelloRedirectLoop {
 
     public static void main(String[] args) throws Exception {
         ServerSocket server = new ServerSocket(8080);
+        int laskuri = 1;
+
+        while (true) {
+            Socket req = server.accept();
+            System.out.println("Request: " + laskuri);
+            laskuri++;
+
+            Scanner s = new Scanner(req.getInputStream());
+
+            String p = s.nextLine();
+
+            if (p.contains("quit")) {
+                break;
+            }
+
+            PrintWriter pw = new PrintWriter(req.getOutputStream());
+            pw.println("HTTP/1.1 302 Found");
+            pw.println("Location: http://localhost:8080/");
+            pw.flush();
+
+            pw.close();
+            req.close();
+            s.close();
+        }
 
     }
+
+
 }
