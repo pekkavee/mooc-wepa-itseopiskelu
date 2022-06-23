553d70046bb5a4fd5e1e6591953a55cdec063da8
pekkavee <pekkatapiovenalainen@gmail.com>
2022-06-23 14:58:00 +0300

    toka

--- osa01-Osa01_02.HelloServer/src/main/java/HelloServer.java
+++ osa01-Osa01_02.HelloServer/src/main/java/HelloServer.java
@@ -1,5 +1,6 @@
 
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -11,6 +12,29 @@ public class HelloServer {
 
     public static void main(String[] args) throws Exception {
         ServerSocket server = new ServerSocket(8080);
+        int laskuri = 1;
+
+        while (true) {
+            Socket socket = server.accept();
+            System.out.println("Laskuri: " + laskuri);
+            laskuri++;
+
+            Scanner lukija = new Scanner(socket.getInputStream());
+            if (lukija.hasNextLine() && lukija.nextLine().contains("quit")) {
+                break;
+            }
+
+            PrintWriter kirjoittaja = new PrintWriter(socket.getOutputStream());
+
+            kirjoittaja.println("HTTP/1.1 200 OK");
+            kirjoittaja.println("");
+            Files.lines(Paths.get("index.html")).forEach(line -> kirjoittaja.println(line));
+            kirjoittaja.flush();
+
+            kirjoittaja.close();
+            socket.close();
+            lukija.close();
+        }
 
     }
 }
